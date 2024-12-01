27b36f6875550082a062a9ad755bf2d8d485d4ed
BlueyHeeler <tairaerick@gmail.com>
2024-12-01 12:38:48 -0300

    Refatoracao_1
    Refatorando o primeiro teste

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -1,3 +1,4 @@
+// Copyright 2024 <Erick>
 #include "velha.hpp"
 #include<bits/stdc++.h>
 using namespace std;
@@ -5,15 +6,11 @@ using namespace std;
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-
-
 TEST_CASE( "Testa velha", "[single-file]" ) {
-	vector< vector<int>> teste1= {   { 2, 0, 1 },
-									 { 2, 0, 1 },
-									 { 0, 2, 1 }
-								 };
-    REQUIRE( VerificaVelha(teste1) == 1 );
-
-
-} 
- 
+	vector< vector<int> > teste1= {
+	{ 2, 0, 1 },
+	{ 2, 0, 1 },
+	{ 0, 2, 1 }
+	};
+	REQUIRE( VerificaVelha(teste1) == 1 );
+}
--- velha.cpp
+++ velha.cpp
@@ -2,31 +2,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int VerificaVelha( vector<vector<int>> velha )
-{
-	int diagonal;
-	int coluna;
-	int fileira;
-	for(int jogador = 1; jogador<=2; jogador++){
-		for(int i = 0; i<3; i++){
-			for(int j = 0; j<3; j++){
-				//Verifica Diagonal
-				if(i == j && velha[i][j] == jogador)
-					diagonal += 1;
-				//Verifica Fileira
-				if(velha[i][j] == jogador)
-					fileira += 1;
-				//Verifica Coluna
-				if(velha[j][i] == jogador)
-					coluna += 1;
+int VerificaDiagonal(int row, int column,
+					 vector<vector<int>> velha, int jogador) {
+	return (row == column && velha[row][column] == jogador);
+}
+
+int VerificaFileira(int row, int column,
+					vector<vector<int>> velha, int jogador) {
+	return velha[row][column] == jogador;
+}
+
+int VerificaColuna(int column, int row,
+				   vector<vector<int>> velha, int jogador) {
+	return velha[column][row] == jogador;
+}
+
+int VerificaVelha(vector<vector<int>> velha) {
+	int diagonal = 0;
+	int coluna = 0;
+	int fileira = 0;
+
+	for (int jogador = 1; jogador <= 2; jogador++) {
+		for (int row = 0; row < 3; row++) {
+			for (int column = 0; column < 3; column++) {
+				if (VerificaDiagonal(row, column, velha, jogador))
+					diagonal++;
+
+				if (VerificaFileira(row, column, velha, jogador))
+					fileira++;
+
+				if (VerificaColuna(column, row, velha, jogador))
+					coluna++;
 			}
-			if(fileira == 3 || coluna == 3)
+
+			if (fileira == 3 || coluna == 3)
 				return jogador;
+
 			coluna = 0;
 			fileira = 0;
 		}
+
+		if (diagonal == 3)
+			return jogador;
+
+		diagonal = 0;
 	}
-	return 0; /*!< retorna zero para teste */
+
+	return 0;
 }
+
 
 
